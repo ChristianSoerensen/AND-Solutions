35834bae81a9b6bc080923982b6fc2d1dd06cd7a
KasperKnop <kasperknop@gmail.com>
2018-09-03 11:37:26 +0200

    Exercise 1 Activity

---
+++ app/src/main/java/com/example/kaspe/courseintroduction/Exercise1.java
@@ -0,0 +1,13 @@
+package com.example.kaspe.courseintroduction;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Exercise1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.exercise1_6);
+    }
+}
